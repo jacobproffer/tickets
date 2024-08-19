79354c38910bbdeffd03c56aab3c7f7c6d92c8e1
jacobproffer <jacobhardingproffer@gmail.com>
2024-08-19 19:36:37 -0400

    Implement: Navigation

--- themes/tickets/assets/js/app.js
+++ themes/tickets/assets/js/app.js
@@ -1,9 +1,66 @@
-// Cache DOM elements
+const mainHeader = document.querySelector('[data-header]');
+const mainNavigation = document.querySelector('[data-navigation]');
+const mainNavigationList = document.querySelector('[data-navigation-list]');
+const mobileNavigationTrigger = document.querySelector('[data-navigation-toggle]');
+const mobileNavigation = document.querySelector('[data-navigation-list]');
 const fadeIns = document.querySelectorAll('.gsap-fade-in');
 const counters = document.querySelectorAll('[data-counter]');
 
 // GSAP ScrollTrigger plugin registration
 gsap.registerPlugin(ScrollTrigger);
+
+/**
+ * Trap focus within navigation
+ */
+function navigationFocus() {
+  const focusableElements = mainNavigation.querySelectorAll('a[href], button');
+  const firstFocusableElement = focusableElements[0];
+  const lastFocusableElement = focusableElements[focusableElements.length - 1];
+
+  mainNavigation.addEventListener('keydown', function (e) {
+    if (e.target === firstFocusableElement && e.key === 'Tab' && e.shiftKey) {
+      e.preventDefault();
+      lastFocusableElement.focus();
+    } else if (e.target === lastFocusableElement && e.key === 'Tab' && !e.shiftKey) {
+      e.preventDefault();
+      firstFocusableElement.focus();
+    }
+  });
+}
+
+/**
+ * Close navigation if escape key is pressed
+ */
+function handleEscape() {
+  document.addEventListener('keyup', function (e) {
+    const escape = e.key;
+
+    if (escape === 'Escape' && mobileNavigation.classList.contains('open')) {
+      mobileNavigationTrigger.setAttribute('aria-expanded', 'false');
+      mobileNavigationTrigger.focus();
+      mobileNavigation.classList.remove('open');
+      mainHeader.classList.remove('main-header--navigation-open');
+      mobileNavigationTrigger.innerHTML = "Open Menu";
+    }
+  });
+}
+
+mobileNavigationTrigger.addEventListener("click", function () {
+  mainHeader.classList.toggle('main-header--navigation-open');
+  mainNavigationList.classList.toggle("open")
+  this.classList.toggle("nav-open");
+
+  navigationFocus();
+  handleEscape();
+
+  if (mainNavigationList.classList.contains('open')) {
+    this.setAttribute('aria-expanded', 'true');
+    this.innerHTML = "Close Menu";
+  } else {
+    this.setAttribute('aria-expanded', 'false');
+    this.innerHTML = "Open Menu";
+  }
+});
 
 // Function to handle counter animations
 const animateCounters = () => {
